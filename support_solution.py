ef4834a1df5c1f63a87eea2e108840d3d523be00
jesus-333 <albi25.zanca@gmail.com>
2022-03-28 13:04:35 +0200

    Work on solution

--- support_solution.py
+++ support_solution.py
@@ -1,6 +1,8 @@
 import numpy as np
 
-def create_compilation_tree(file_info, file_target):
+#%% Function related to data rearrangement 
+
+def create_compilation_tree(files_info, file_target):
     """
     INPUT: dictionary of the files (file info) and the name of the target file (file target) 
     The function create a "tree" of file that you need to compile in order to compile the target file.
@@ -11,7 +13,8 @@ def create_compilation_tree(file_info, file_target):
     layer_list  = [[file_target]]
     
     # Parent files for the layer that I'm analyzing in this moment
-    current_layer_parent_files = file_info[file_target]['dependencies_list']
+    current_layer_parent_files = []
+    current_layer_parent_files += files_info[file_target]['dependencies_list']
     
     # Eventual parents of the files of the next layer (i.e. the parents of the files in current_layer_parent_files) 
     next_layer_parent_files = []
@@ -26,3 +29,73 @@ def create_compilation_tree(file_info, file_target):
         tmp_file = current_layer_parent_files.pop(0)
         
         tmp_layer.append(tmp_file)
+        
+        next_layer_parent_files += files_info[tmp_file]['dependencies_list']
+        
+        if(len(current_layer_parent_files) == 0):
+            # Add the layer to the list of layers
+            layer_list.append(tmp_layer)
+            
+            # Remove duplicates (if there are) and create new list of parents
+            current_layer_parent_files = list(set(next_layer_parent_files))
+            
+            # Clean old lists
+            next_layer_parent_files = []
+            tmp_layer = []
+            
+    return layer_list
+
+    
+def sort_target_by_attribute(target_list, attribute = 'poinst', reverse = True):
+    """
+    "Sort" the key of dictionary so that when they are cycled the first one is the one with the hight attribute etc
+    """
+    
+    return {k: v for k, v in sorted(target_list.items(), key=lambda item: item[1][attribute], reverse = reverse)}
+
+
+def create_compilation_tree_per_targets(files_info, targets):
+    # For each target contain its "list" of files to compile
+    compilation_tree_per_target = {}
+
+    for tmp_target in targets: 
+        compilation_tree_per_target[tmp_target] = create_compilation_tree(files_info, tmp_target) 
+        
+    return compilation_tree_per_target
+
+#%% 
+
+def compute_solution(C, files_info, targets, compilation_tree_per_target):
+    t = 0
+    
+    current_elaboration = []
+    tmp_compilation_trees = []
+    i = 0
+    for target in targets:
+        tmp_compilation_trees.append(compilation_tree_per_target[target])
+    
+    while(True):
+        t += 1
+        
+        
+
+#%% Debug/Visualization function
+
+def compute_layers_string(layer_list):
+    """
+    Given layer list create a string where each layer is more indented than the previous one.
+    Used for debug.
+    """
+    
+    layers_string = ""
+    tabulation = ""
+    for layer in layer_list:
+        for file in layer:
+            layers_string += tabulation + str(file) + "\n"
+        tabulation += "\t"
+        
+    return layers_string
+
+
+def show_layers(layer_list):
+    print(compute_layers_string(layer_list))
--- test_1.py
+++ test_1.py
@@ -2,8 +2,65 @@ import numpy as np
 
 from support_data_extraction import get_file, extract_info
 
+from support_solution import create_compilation_tree, sort_target_by_attribute, create_compilation_tree_per_targets
+from support_solution import compute_layers_string, show_layers
+
 #%%
 
+# Load the data
 data = get_file()
 
-C, T, S, file_info, targets =  extract_info(data[1])
+# Extract info
+C, T, S, files_info, targets = extract_info(data[0])
+
+# Sort target by attribute
+attribute = 'points' 
+# attribute = 'deadline'
+targets = sort_target_by_attribute(targets, attribute)
+
+#%%
+
+# Visualize a compilation tree of a target (For debugging)
+targest_keys = list(targets.keys())
+tmp_target = targest_keys[0]
+show_layers( create_compilation_tree(files_info, tmp_target))
+
+# Create the compilation tree for each target file
+compilation_tree_per_target = create_compilation_tree_per_targets(files_info, targets)
+
+
+#%%
+
+
+# Time variable
+t = 0
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Find the first file to compile
+
+# Variable to save the current files in compilation 
+current_elaboration = []
+tmp_compilation_tree_list = []
+i = 0
+
+# Select the first S targets
+for target in targets:
+    tmp_compilation_tree_list.append(compilation_tree_per_target[target] + [])
+    i += 1
+    
+    if(i >= S): break
+
+# Delete i variable (for safety)
+del i
+
+# Chcek if there are "last layer" with only 1 file in the various compilation tree
+# If there are for that tree I'm forced to start the compilation from that file
+for tmp_compilation_tree in tmp_compilation_tree_list:
+    if(len(tmp_compilation_tree[-1]) == 1): current_elaboration.append(tmp_compilation_tree[-1][0])
+    
+for i in range(S - len(current_elaboration)):
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# while(True):
+#     t += 1
